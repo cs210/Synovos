ba59498caa4b2a8fc47779640932f28d485d6a5f
Nikita Demir <nikitde1@gmail.com>
2020-03-08 22:01:07 -0700

    Got onboarding flow to ask user to create the json object

--- CS210_heatmap_V1/components/onboarding/onboarding.jsx
+++ CS210_heatmap_V1/components/onboarding/onboarding.jsx
@@ -4,22 +4,90 @@ import {
 } from '@material-ui/core';
 
 class Onboarding extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {buildingName: '', floorName: '', roomName: '', sensorName:'', jsonData:{}};
 
-    render() {
-        return (
-            <Typography variant="body1">
-                Welcome to your Onboarding Prototype! This <a href="https://material-ui.com/demos/paper/">Paper</a> component
-                displays the main content of the application. The {"sm={9}"} prop in
-                the <a href="https://material-ui.com/layout/grid/">Grid</a> item component makes it responsively
-                display 9/12 of the window. The Switch component enables us to conditionally render different
-                components to this part of the screen. You don&apos;t need to display anything here on the homepage,
-                so you should delete this Route component once you get started.
-            </Typography>
-        );
+    this.handleBuildingChange = this.handleBuildingChange.bind(this);
+    this.handleFloorChange = this.handleFloorChange.bind(this);
+    this.handleRoomChange = this.handleRoomChange.bind(this);
+    this.handleSensorChange = this.handleSensorChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleBuildingChange(event) {
+    this.setState({buildingName: event.target.value});
+  }
+
+  handleFloorChange(event) {
+    this.setState({floorName: event.target.value});
+  }
+
+  handleRoomChange(event) {
+    this.setState({roomName: event.target.value});
+  }
+
+  handleSensorChange(event) {
+    this.setState({sensorName: event.target.value});
+  }
+
+  handleSubmit(event) {
+    // Do the creation of the object
+    if (this.state.buildingName != "") {
+      this.state.jsonData[this.state.buildingName] = this.state.jsonData[this.state.buildingName] || {};
+      if (this.state.floorName != "") {
+        this.state.jsonData[this.state.buildingName][this.state.floorName] = this.state.jsonData[this.state.buildingName][this.state.floorName] || {};
+        if (this.state.roomName != "") {
+          console.log(this.state.jsonData[this.state.buildingName][this.state.floorName][this.state.roomName])
+          if (this.state.jsonData[this.state.buildingName][this.state.floorName][this.state.roomName] == undefined) {
+            this.state.jsonData[this.state.buildingName][this.state.floorName][this.state.roomName] = new Set();
+          }
+          this.state.jsonData[this.state.buildingName][this.state.floorName][this.state.roomName] = new Set(this.state.jsonData[this.state.buildingName][this.state.floorName][this.state.roomName])
+          if (this.state.sensorName != ""){
+            this.state.jsonData[this.state.buildingName][this.state.floorName][this.state.roomName].add(this.state.sensorName)
+          }
+          this.state.jsonData[this.state.buildingName][this.state.floorName][this.state.roomName] = Array.from(this.state.jsonData[this.state.buildingName][this.state.floorName][this.state.roomName])
+        }
+      }
     }
+    console.log(this.state.jsonData);
+    this.setState({jsonData: this.state.jsonData});
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label>
+            Building:
+            <input type="text" value={this.state.buildingName} onChange={this.handleBuildingChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Floor:
+            <input type="text" value={this.state.floorName} onChange={this.handleFloorChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Room:
+            <input type="text" value={this.state.roomName} onChange={this.handleRoomChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Sensor:
+            <input type="text" value={this.state.sensorName} onChange={this.handleSensorChange} />
+          </label>
+        </div>
+        <input type="submit" value="Submit" />
+        <div> {JSON.stringify(this.state.jsonData)}
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Onboarding;
