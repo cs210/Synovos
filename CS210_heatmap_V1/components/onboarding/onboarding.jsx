5f8cc9e68ea224a2dee40caaa12c3db40ae35588
Nikita Demir <nikitde1@gmail.com>
2020-03-08 23:54:35 -0700

    Onboarding is ready

--- CS210_heatmap_V1/components/onboarding/onboarding.jsx
+++ CS210_heatmap_V1/components/onboarding/onboarding.jsx
@@ -1,45 +1,71 @@
 import React from 'react';
 import {
     Typography,
+    TextField
 } from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
 
 class Onboarding extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {buildingName: '', floorName: '', roomName: '', sensorName:'', jsonData:{}};
+    this.state = {buildingName: '',
+                  floorName: '',
+                  roomName: '',
+                  sensorName: '',
+                  jsonData: {},
+                  buildingOptions: [],
+                  floorOptions: [],
+                  roomOptions: [],
+                  sensorOptions: [],
+                 };
 
     this.handleBuildingChange = this.handleBuildingChange.bind(this);
     this.handleFloorChange = this.handleFloorChange.bind(this);
     this.handleRoomChange = this.handleRoomChange.bind(this);
     this.handleSensorChange = this.handleSensorChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateOptions = this.updateOptions.bind(this)
+
   }
 
 
-  handleBuildingChange(event) {
-    this.setState({buildingName: event.target.value});
+  updateOptions() {
+    this.setState({buildingOptions: Object.keys(this.state.jsonData)});
+    if (this.state.buildingName != "") {
+      this.setState({floorOptions: this.state.jsonData[this.state.buildingName] == undefined ? [] : Object.keys(this.state.jsonData[this.state.buildingName])});
+      if (this.state.floorName != "") {
+        this.setState({roomOptions: (this.state.jsonData[this.state.buildingName] != undefined && this.state.jsonData[this.state.buildingName][this.state.floorName] != undefined) ? Object.keys(this.state.jsonData[this.state.buildingName][this.state.floorName]) : []});
+        if (this.state.roomName != "") {
+          this.setState({sensorOptions: (this.state.jsonData[this.state.buildingName] != undefined && this.state.jsonData[this.state.buildingName][this.state.floorName] != undefined && this.state.jsonData[this.state.buildingName][this.state.floorName][this.state.roomName] != undefined) ? this.state.jsonData[this.state.buildingName][this.state.floorName][this.state.roomName] : []});
+        }
+      }
+    }
   }
 
-  handleFloorChange(event) {
-    this.setState({floorName: event.target.value});
+  handleBuildingChange(event, value) {
+    this.setState({buildingName: value}, () => { this.updateOptions(); });
   }
 
-  handleRoomChange(event) {
-    this.setState({roomName: event.target.value});
+  handleFloorChange(event, value) {
+    this.setState({floorName: value}, () => { this.updateOptions(); });
   }
 
-  handleSensorChange(event) {
-    this.setState({sensorName: event.target.value});
+  handleRoomChange(event, value) {
+    this.setState({roomName: value}, () => { this.updateOptions(); });
+  }
+
+  handleSensorChange(event, value) {
+    this.setState({sensorName: value}, () => { this.updateOptions(); });
   }
 
   handleSubmit(event) {
     // Do the creation of the object
     if (this.state.buildingName != "") {
-      this.state.jsonData[this.state.buildingName] = this.state.jsonData[this.state.buildingName] || {};
+      this.state.jsonData[this.state.buildingName] = this.state.jsonData[this.state.buildingName] != undefined ? this.state.jsonData[this.state.buildingName] : {};
       if (this.state.floorName != "") {
-        this.state.jsonData[this.state.buildingName][this.state.floorName] = this.state.jsonData[this.state.buildingName][this.state.floorName] || {};
+        this.state.jsonData[this.state.buildingName][this.state.floorName] = this.state.jsonData[this.state.buildingName] != undefined && this.state.jsonData[this.state.buildingName][this.state.floorName] != undefined ? this.state.jsonData[this.state.buildingName][this.state.floorName] : {};
         if (this.state.roomName != "") {
-          console.log(this.state.jsonData[this.state.buildingName][this.state.floorName][this.state.roomName])
           if (this.state.jsonData[this.state.buildingName][this.state.floorName][this.state.roomName] == undefined) {
             this.state.jsonData[this.state.buildingName][this.state.floorName][this.state.roomName] = new Set();
           }
@@ -53,34 +79,55 @@ class Onboarding extends React.Component {
     }
     console.log(this.state.jsonData);
     this.setState({jsonData: this.state.jsonData});
+    this.updateOptions();
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <div>
-          <label>
-            Building:
-            <input type="text" value={this.state.buildingName} onChange={this.handleBuildingChange} />
-          </label>
+        <div style={{ width: 300 }}>
+        <Autocomplete
+          freeSolo
+          options={this.state.buildingOptions}
+          renderInput={params => (
+            <TextField {...params} label="Building" margin="normal" variant="outlined" />
+          )}
+          onInputChange={this.handleBuildingChange}
+          value={this.state.buildingName}
+        />
         </div>
-        <div>
-          <label>
-            Floor:
-            <input type="text" value={this.state.floorName} onChange={this.handleFloorChange} />
-          </label>
+        <div style={{ width: 300 }}>
+        <Autocomplete
+          freeSolo
+          options={this.state.floorOptions}
+          renderInput={params => (
+            <TextField {...params} label="Floor" margin="normal" variant="outlined" />
+          )}
+          onInputChange={this.handleFloorChange}
+          value={this.state.floorName}
+        />
         </div>
-        <div>
-          <label>
-            Room:
-            <input type="text" value={this.state.roomName} onChange={this.handleRoomChange} />
-          </label>
+        <div style={{ width: 300 }}>
+        <Autocomplete
+          freeSolo
+          options={this.state.roomOptions}
+          renderInput={params => (
+            <TextField {...params} label="Room" margin="normal" variant="outlined" />
+          )}
+          onInputChange={this.handleRoomChange}
+          value={this.state.roomName}
+        />
         </div>
-        <div>
-          <label>
-            Sensor:
-            <input type="text" value={this.state.sensorName} onChange={this.handleSensorChange} />
-          </label>
+        <div style={{ width: 300 }}>
+        <Autocomplete
+          freeSolo
+          options={this.state.sensorOptions}
+          renderInput={params => (
+            <TextField {...params} label="Sensor" margin="normal" variant="outlined" />
+          )}
+          onInputChange={this.handleSensorChange}
+          value={this.state.sensorName}
+        />
         </div>
         <input type="submit" value="Submit" />
         <div> {JSON.stringify(this.state.jsonData)}
