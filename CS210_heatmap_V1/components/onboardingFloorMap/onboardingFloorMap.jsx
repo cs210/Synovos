55539a6558b6087f34f86d7bfaf612b04093ad09
Nikita Demir <nikitde1@gmail.com>
2020-03-10 16:56:34 -0700

    Stupid stupid bugs with konva

--- CS210_heatmap_V1/components/onboardingFloorMap/onboardingFloorMap.jsx
+++ CS210_heatmap_V1/components/onboardingFloorMap/onboardingFloorMap.jsx
@@ -3,8 +3,34 @@ import {
     Typography,
     Button
 } from '@material-ui/core';
-import {SketchField, Tools} from 'react-sketch';
+import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
 
+
+class ColoredRect extends React.Component {
+  state = {
+    color: 'green'
+  };
+  handleClick = () => {
+    this.setState({
+      color: Konva.Util.getRandomColor()
+    });
+  };
+  render() {
+    return (
+      <Rect
+        x={20}
+        y={20}
+        width={50}
+        height={50}
+        fill={this.state.color}
+        shadowBlur={5}
+        onClick={this.handleClick}
+      />
+    );
+  }
+}
 
 class OnboardingFloorMap extends React.Component {
   constructor(props) {
@@ -13,34 +39,82 @@ class OnboardingFloorMap extends React.Component {
                   floorName: '',
                   roomName: '',
                   jsonData: {},
+                  rectange: null,
                  };
 
-    this.sketchField = React.createRef();
+    this.handleMouseDown = this.handleMouseDown.bind(this)
+    this.handleMouseUp = this.handleMouseUp.bind(this)
+    this.handleMouseMove = this.handleMouseMove.bind(this)
 
-    this.handleImageUpload = this.handleImageUpload.bind(this)
+    this.stageRef = React.createRef();
+    this.layerRef = React.createRef();
+    this._drawing = false;
   }
 
-  handleImageUpload() {
-    this.sketchField.current.setBackgroundFromDataUrl(
-      "https://library.gwu.edu/sites/default/files/communications/gelman-2nd-02.png",
-      {
-            width:window.innerWidth,
-            height:window.innerHeight,
-          });
+  handleMouseDown(event) {
+    this._drawing = true;
+    var pos = this.stageRef.current.getPointerPosition();
+    this.rectangle = new Konva.Rect({   
+      x: pos.x,
+      y: pos.y,
+      width: 50,
+      height: 50,
+      fill: 'black',
+      stroke: 'black',
+      strokeWidth: 4
+    });
+    this.layerRef.current.add(this.rectangle);
+
+    this.oldX = pos.x
+    this.oldY = pos.y
   }
+
+  handleMouseUp() {
+    this._drawing = false;
+  }
+
+  handleMouseMove() {
+    if (!this._drawing) {
+        return;
+    }
+
+    var pos = this.stageRef.current.getPointerPosition();
+    console.log(this.oldX)
+    console.log(pos[0])
+    var newRectangle = new Konva.Rect({   
+      x: this.oldX,
+      y: this.oldY,
+      width: pos[0] - this.oldX,
+      height: pos[1] - this.oldY,
+      fill: 'black',
+      stroke: 'black',
+      strokeWidth: 4
+    });
+    this.layerRef.current.remove(this.rectangle);
+    this.layerRef.current.add(newRectangle);
+    this.rectangle = newRectangle;
+    // this.rectangle.width = pos.x - this.rectangle.x 
+    // this.rectangle.height = pos.y - this.rectangle.y
+    this.layerRef.current.draw();
+  }
+
 
   render() {
     return (
       <div>
       <Button variant="contained" onClick={this.handleImageUpload}>Upload Image</Button>
-      <SketchField 
-                         tool={Tools.Rectangle}
-                         ref={this.sketchField}
-                         lineColor='red'
-                         fillColor='red'
-                         lineWidth={3}
-                               sketchWidth={600}
-      sketchHeight={600}/>
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onContentMousedown={this.handleMouseDown}
+        onContentMousemove={this.handleMouseMove}
+        onContentMouseup={this.handleMouseUp}
+        ref={this.stageRef}
+      >
+
+        <Layer ref={this.layerRef}>
+        </Layer>
+      </Stage>
       </div>
     );
   }
