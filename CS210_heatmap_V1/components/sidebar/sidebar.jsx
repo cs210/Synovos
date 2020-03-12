235f314e5b83fb5d825b97712771ed95f30be6a7
Jose Giron <30703536+jmgiron98@users.noreply.github.com>
2020-03-12 14:07:52 -0700

    Removed the underline for nav elements

--- CS210_heatmap_V1/components/sidebar/sidebar.jsx
+++ CS210_heatmap_V1/components/sidebar/sidebar.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import {
   List,
   ListItem,
-  ListItemText,
   Typography,
 }
 from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
 import './sidebar.css';
-import axios from 'axios';
 
 class Sidebar extends React.Component {
     constructor(props) {
@@ -24,22 +22,34 @@ class Sidebar extends React.Component {
                 </Typography>
                 <List component="nav">
                     <ListItem key={1} button >
-                        <NavLink to="/">
+                        <NavLink
+                            to="/"
+                            style={{ textDecoration: 'none' }}
+                        >
                             Home
                         </NavLink>
                     </ListItem>
                     <ListItem key={2} button >
-                        <NavLink to="/Heatmap">
+                        <NavLink
+                            to="/Heatmap"
+                            style={{ textDecoration: 'none' }}
+                        >
                             Heat Map
                         </NavLink>
                     </ListItem>
                     <ListItem key={3} button >
-                        <NavLink to="/Listview">
+                        <NavLink
+                            to="/Listview"
+                            style={{ textDecoration: 'none' }}
+                        >
                             List View
                         </NavLink>
                     </ListItem>
                     <ListItem key={4} button >
-                        <NavLink to="/Onboarding">
+                        <NavLink
+                            to="/Onboarding"
+                            style={{ textDecoration: 'none' }}
+                        >
                             Onboarding
                         </NavLink>
                     </ListItem>
